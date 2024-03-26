f68ed4220f18b2d46e11e08aa57441d4c29f6d99
Kornelll <>
2024-03-26 23:47:23 +0500

    set up shadcn-ui

---
+++ README.md
@@ -0,0 +1,9 @@
+# Expense Tracker App
+
+This application has been developed to solve a major problem of budget and expense management which was causing financial stress and anxiety among students. As I examined the survey and research papers and gained knowledge and awareness from user personas, and went through the design thinking process step by step, I proposed a solution to develop an "Expense Tracker App".
+
+## Technologies used:
+
+- Next.js + Typescript
+- Tailwindcss
+- Shadcn UI
---
+++ src/app/page.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image'
+
+const TITLE = 'Welcome Dear!'
+const DESCRIPTION =
+  'Welcome to our Expense Tracker app! Take control of your finances effortlessly with our intuitive and user-friendly platform.'
+
+export default async function Home() {
+  return (
+    <main className="min-h-screen grid lg:grid-cols-2">
+      {/* Left Section */}
+      <section className="grid place-items-center m-10">
+        <div className="grid gap-4 w-[360px]">
+          <h1 className="text-3xl text-center font-extrabold">{TITLE}</h1>
+          <p className="text-sm text-center text-muted-foreground">
+            {DESCRIPTION}
+          </p>
+          {/* AuthForm */}
+        </div>
+      </section>
+
+      {/* Right Section */}
+      <section className="p-4 hidden lg:block">
+        <div className="relative w-full h-full rounded-lg overflow-hidden">
+          <Image
+            src="/signup.jpg"
+            alt="Sign Up"
+            fill
+            className="object-cover"
+          />
+        </div>
+      </section>
+    </main>
+  )
+}
